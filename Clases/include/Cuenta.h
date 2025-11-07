ff658a0d3c6eb24f22c69b13941be98f53df7ebf
Almary ChB <almary.choque@ucsp.edu.pe>
2025-11-07 08:05:15 -0500

    Update Cuenta.h

--- Clases/include/Cuenta.h
+++ Clases/include/Cuenta.h
@@ -1,1 +1,16 @@
+#ifndef CUENTA.H
+#define CUENTA.H
+#include "String_.h"
 
+class Cuenta
+{
+	public:
+	       Cuenta();
+		     Cuenta(int numCuenta[20])
+	private:
+         int numCuenta[20];
+		     int idUsuario;
+		     double saldo;
+
+}
+ #endif // CUENTA.H
