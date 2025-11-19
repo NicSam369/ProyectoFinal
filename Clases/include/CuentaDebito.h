56e7c840a4c90de479280aff385fb291b7bd94c6
Almary ChB <almary.choque@ucsp.edu.pe>
2025-11-19 18:51:36 -0500

    Create CuentaDebito.h

---
+++ Clases/include/CuentaDebito.h
@@ -0,0 +1,12 @@
+#ifndef CUENTA_DEBITO_H
+#define CUENTA_DEBITO_H
+#include "Cuenta.h"
+
+class CuentaDebito : public Cuenta {
+public:
+    CuentaDebito(char numCuenta_[20], char DNI[9], double saldoInicial);
+
+    void mostrarDatos() const override;
+};
+
+#endif
