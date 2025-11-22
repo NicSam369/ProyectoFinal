aa58b8f2269f14f9bcb3e2fa9a2d37a6b80d13b6
Fernanda Gabriela Zuñiga Camacho <149815255+Fernandagzcj@users.noreply.github.com>
2025-11-22 18:45:07 -0500

    Create TransaccionRetiro.h

---
+++ Clases/include/TransaccionRetiro.h
@@ -0,0 +1,35 @@
+#ifndef TRANSACCIONRETIRO_H
+#define TRANSACCIONRETIRO_H
+#include "Transaccion.hpp"
+#include "String_.hpp"
+#include <iostream>
+using namespace std;
+
+class TransaccionRetiro: public Transaccion {
+    protected:
+        String_ tipoRetiro;
+        
+    public:
+        TransaccionRetiro(int tipo, double monto, int idUsuario, char numCuenta[20], String_ tipoRetiro)
+            : Transaccion(tipo, monto, idUsuario, numCuenta)
+        {
+            this->tipoRetiro = tipoRetiro;	
+        }	
+        
+        String_ gettipoRetiro(){
+            return tipoRetiro;
+        }
+        
+        void settipoRetiro(String_ tipoRetiro){
+            this->tipoRetiro = tipoRetiro;
+        }
+        
+        void mostrarRetiro(){
+            mostrarTransaccion();
+            cout << "Retiro: " << tipoRetiro << endl;
+        }
+        
+        ~TransaccionRetiro(){
+        }
+};
+#endif
