7690dd042d3610c84c73a2bbcb6894572f64cc42
Fernanda Gabriela Zuñiga Camacho <149815255+Fernandagzcj@users.noreply.github.com>
2025-11-24 03:55:49 -0500

    Update TransaccionPrestamo.h

--- Clases/include/TransaccionPrestamo.h
+++ Clases/include/TransaccionPrestamo.h
@@ -3,24 +3,41 @@
 #include "Transaccion.h"
 #include "String_.h"
 #include <iostream>
-using namespace std;
 
-class TransaccionPrestamo: public Transaccion {
-    protected:
-        int plazoMeses;
-        double tasaInteres;
-        double cuotaMensual;
-        String_ tipoPrestamo;
+class TransaccionPrestamo : public Transaccion
+{
+protected:
+    int plazoMeses;
+    double tasaInteres;
+    double cuotaMensual;
+    String_ tipoPrestamo;
 
-    public:
-        TransaccionPrestamo();
-        TransaccionPrestamo(int tipo, double monto, int idUsuario,
-                           char numCuenta[20], int plazoMeses, double tasaInteres,
-                           double cuotaMensual, String_ tipoPrestamo)
+public:
+    TransaccionPrestamo();
 
-        void mostrar() const override;
+    TransaccionPrestamo(int tipo, double monto, int idUsuario,
+                        const char numCuenta[20],
+                        int plazoMeses, double tasaInteres,
+                        double cuotaMensual, String_ tipoPrestamo)
+        : Transaccion(tipo, monto, idUsuario, numCuenta)
+    {
+        this->plazoMeses = plazoMeses;
+        this->tasaInteres = tasaInteres;
+        this->cuotaMensual = cuotaMensual;
+        this->tipoPrestamo = tipoPrestamo;
+    }
 
-        ~TransaccionPrestamo();
+    virtual void mostrar() const override {
+        Transaccion::mostrar();
+        cout << "Tipo prestamo: "; tipoPrestamo.print();
+        cout << "\nPlazo: " << plazoMeses
+             << "\nTasa interes: " << tasaInteres
+             << "\nCuota mensual: " << cuotaMensual << endl;
+    }
+
+    virtual ~TransaccionPrestamo();
 };
+
 #endif
+
 
