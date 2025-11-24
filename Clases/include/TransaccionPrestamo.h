52de2f3a232252c68985e1e4306079d4c0de2530
Fernanda Gabriela Zuñiga Camacho <149815255+Fernandagzcj@users.noreply.github.com>
2025-11-24 03:25:50 -0500

    Update TransaccionPrestamo.h

--- Clases/include/TransaccionPrestamo.h
+++ Clases/include/TransaccionPrestamo.h
@@ -17,54 +17,10 @@ class TransaccionPrestamo: public Transaccion {
         TransaccionPrestamo(int tipo, double monto, int idUsuario,
                            char numCuenta[20], int plazoMeses, double tasaInteres,
                            double cuotaMensual, String_ tipoPrestamo)
-            : Transaccion(tipo, monto, idUsuario, numCuenta)
-        {
-            this->plazoMeses = plazoMeses;
-            this->tasaInteres = tasaInteres;
-            this->cuotaMensual = cuotaMensual;
-            this->tipoPrestamo = tipoPrestamo;
-        }
 
-        int getplazoMeses(){
-            return plazoMeses;
-        }
-
-        double gettasaInteres(){
-            return tasaInteres;
-        }
-
-        double getcuotaMensual(){
-            return cuotaMensual;
-        }
-
-        String_ gettipoPrestamo(){
-            return tipoPrestamo;
-        }
-
-        void setplazoMeses(int plazoMeses){
-            this->plazoMeses = plazoMeses;
-        }
-
-        void settasaInteres(double tasaInteres){
-            this->tasaInteres = tasaInteres;
-        }
-
-        void setcuotaMensual(double cuotaMensual){
-            this->cuotaMensual = cuotaMensual;
-        }
-
-        void settipoPrestamo(String_ tipoPrestamo){
-            this->tipoPrestamo = tipoPrestamo;
-        }
-
-        void mostrarPrestamo(){
-            mostrarTransaccion();
-            cout << "Prestamo: " << tipoPrestamo.str_ << endl;
-            cout << "Plazo: " << plazoMeses << endl;
-            cout << "Tasa Interes: " << tasaInteres << endl;
-            cout << "Cuota Mensual: " << cuotaMensual << endl;
-        }
+        void mostrar() const override;
 
         ~TransaccionPrestamo();
 };
 #endif
+
