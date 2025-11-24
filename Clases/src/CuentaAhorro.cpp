b2aa671148b66398a9f7d651d95b7c271962729f
Almary ChB <almary.choque@ucsp.edu.pe>
2025-11-24 00:53:31 -0500

    Create CuentaAhorro.cpp

---
+++ Clases/src/CuentaAhorro.cpp
@@ -0,0 +1,13 @@
+#ifndef CUENTADEBITO_H
+#define CUENTADEBITO_H
+#include "Cuenta.h"
+
+class CuentaDebito : public Cuenta
+{
+    public:
+         CuentaDebito(char numCuenta_[20], char DNI[9], double saldoInicial);
+
+         void mostrarDatos() const override;
+};
+
+#endif //CUENTADEBITO_H
