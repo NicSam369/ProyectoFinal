8215f9219a3c9a0f11080ca250ead2012c4b60de
Fernanda Gabriela Zuñiga Camacho <149815255+Fernandagzcj@users.noreply.github.com>
2025-11-24 03:27:34 -0500

    Update TransaccionDeposito.h

--- Clases/include/TransaccionDeposito.h
+++ Clases/include/TransaccionDeposito.h
@@ -13,35 +13,11 @@ class TransaccionDeposito: public Transaccion {
     public:
         TransaccionDeposito();
 		TransaccionDeposito(int tipo, double monto, int idUsuario, char numCuenta[20], String_ tipoDeposito, String_ depositante)
-			:Transaccion(tipo, monto, idUsuario, numCuenta)
-		{
-			this->tipoDeposito = tipoDeposito;
-			this->depositante = depositante;
-		}
 
-		String_ gettipoDeposito(){
-			return tipoDeposito;
-		}
-
-		String_ getdepositante(){
-			return depositante;
-		}
-
-		void settipoDeposito(String_ tipoDeposito){
-			this-> tipoDeposito = tipoDeposito;
-		}
-
-		void setdepositante(String_ depositante){
-			this->depositante = depositante;
-		}
-
-		void mostrarDeposito(){
-			mostrarTransaccion();
-			cout << "Deposito: " << tipoDeposito.str_ << endl;
-			cout << "Depositante: " << depositante.str_ << endl;
-		}
+		void mostrar() const override;
 
 		~TransaccionDeposito();
 };
 
 #endif
+
