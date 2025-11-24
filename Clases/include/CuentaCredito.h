49122638ebafe4e5c37ee70b616679b800dfbfb0
Almary ChB <almary.choque@ucsp.edu.pe>
2025-11-24 00:52:18 -0500

    Update CuentaCredito.h

--- Clases/include/CuentaCredito.h
+++ Clases/include/CuentaCredito.h
@@ -2,15 +2,21 @@
 #define CUENTACREDITO_H
 #include "Cuenta.h"
 
-class CuentaCredito : public Cuenta 
+class CuentaCredito : public Cuenta
 {
     public:
-       CuentaCredito(char numCuenta_[20], int idUsuario, double saldoInicial, double limtite); 
+       CuentaCredito(char numCuenta_[20], int idUsuario, double saldoInicial, double deudaActual, double Interes);
 
        bool retirar(double monto) override;
        void mostrarDatos() const override;
+       void aplicarInteres();
+
     protected:
-       double limiteCredito; //con lo que nos de el banco dependiendo a la cuenta
+        double deuda(); //DEUDA ACTUAL
+
+   //   double limiteCredito; //con lo que nos de el banco dependiendo a la cuenta
+
 };
 
 #endif // CUENTA_CREDITO_H
+
