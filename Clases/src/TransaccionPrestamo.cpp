94fd32d863be40816e6c55e0c12da64565a35d53
Fernanda Gabriela Zuñiga Camacho <149815255+Fernandagzcj@users.noreply.github.com>
2025-11-27 15:10:56 -0500

    Update TransaccionPrestamo.cpp

--- Clases/src/TransaccionPrestamo.cpp
+++ Clases/src/TransaccionPrestamo.cpp
@@ -1,11 +1,25 @@
 #include "TransaccionPrestamo.h"
+#include <iostream>
+using namespace std;
 
-TransaccionPrestamo::TransaccionPrestamo()
-{
-    //ctor
+TransaccionPrestamo::TransaccionPrestamo() : Transaccion() {
+    plazoMeses = 0;
+    tasaInteres = 0.0;
+    cuotaMensual = 0.0;
+    tipoCliente = 0;
 }
 
-TransaccionPrestamo::~TransaccionPrestamo()
-{
-    //dtor
+double TransaccionPrestamo::calcularTasa(int tipoCliente) const {
+    if (tipoCliente == 3) {
+        return 0.08;
+    } else if (tipoCliente == 2) {
+        return 0.12;
+    } else if (tipoCliente == 1) {
+        return 0.16;
+    } else {
+        return 0.20;
+    }
+}
+
+TransaccionPrestamo::~TransaccionPrestamo() {
 }
