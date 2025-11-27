f509c12452839688362eaebba5acca795ff32abe
Fernanda Gabriela Zuñiga Camacho <149815255+Fernandagzcj@users.noreply.github.com>
2025-11-27 15:10:05 -0500

    Update TransaccionPrestamo.h

--- Clases/include/TransaccionPrestamo.h
+++ Clases/include/TransaccionPrestamo.h
@@ -3,42 +3,43 @@
 #include "Transaccion.h"
 #include "String_.h"
 #include <iostream>
+using namespace std;
 
-class TransaccionPrestamo : public Transaccion
-{
+class TransaccionPrestamo : public Transaccion {
 protected:
     int plazoMeses;
     double tasaInteres;
     double cuotaMensual;
     String_ tipoPrestamo;
+    int tipoCliente;
+
+    double calcularTasa(int tipoCliente) const;
 
 public:
     TransaccionPrestamo();
 
-    TransaccionPrestamo(int tipo, double monto, int idUsuario,
-                        const char numCuenta[20],
-                        int plazoMeses, double tasaInteres,
-                        double cuotaMensual, String_ tipoPrestamo)
+    TransaccionPrestamo(int tipo, double monto, int idUsuario, const char numCuenta[20],
+                        int plazoMeses, double cuotaMensual, String_ tipoPrestamo,
+                        int tipoCliente)
         : Transaccion(tipo, monto, idUsuario, numCuenta)
     {
         this->plazoMeses = plazoMeses;
-        this->tasaInteres = tasaInteres;
         this->cuotaMensual = cuotaMensual;
         this->tipoPrestamo = tipoPrestamo;
+        this->tipoCliente = tipoCliente;
+        this->tasaInteres = calcularTasa(tipoCliente);
     }
 
     virtual void mostrar() const override {
         Transaccion::mostrar();
         cout << "Prestamo: "; tipoPrestamo.print();
-        cout << "\nPlazo: " << plazoMeses
-             << "\nTasa interes: " << tasaInteres
-             << "\nCuota mensual: " << cuotaMensual << endl;
+        cout << "\nCliente Tipo: " << tipoCliente;
+        cout << "\nPlazo: " << plazoMeses << " meses";
+        cout << "\nTasa interes: " << tasaInteres * 100.0 << "% anual";
+        cout << "\nCuota mensual: " << cuotaMensual << endl;
     }
 
     virtual ~TransaccionPrestamo();
 };
 
 #endif
-
-
-
