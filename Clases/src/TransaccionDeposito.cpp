18cc12bc90e2ac2b52db7f6415119143e3d8eb3f
Fernanda Gabriela Zuñiga Camacho <149815255+Fernandagzcj@users.noreply.github.com>
2025-11-27 15:11:32 -0500

    Update TransaccionDeposito.cpp

--- Clases/src/TransaccionDeposito.cpp
+++ Clases/src/TransaccionDeposito.cpp
@@ -1,11 +1,12 @@
 #include "TransaccionDeposito.h"
+#include <iostream>
+using namespace std;
 
-TransaccionDeposito::TransaccionDeposito()
-{
-    //ctor
+TransaccionDeposito::TransaccionDeposito() : Transaccion() {
+    for(int i = 0; i < 20; i++) {
+        numCuentaOrigen[i] = '0';
+    }
 }
 
-TransaccionDeposito::~TransaccionDeposito()
-{
-    //dtor
+TransaccionDeposito::~TransaccionDeposito() {
 }
