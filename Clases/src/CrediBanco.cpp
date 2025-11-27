b8a5de1ab0d2cfbae775e77b2ea60bd82ba059ff
damiancabrera123 <damian.cabrera@ucsp.edu.pe>
2025-11-27 08:16:32 -0500

    Update CrediBanco.cpp

--- Clases/src/CrediBanco.cpp
+++ Clases/src/CrediBanco.cpp
@@ -21,18 +21,24 @@ void Credibanco:: caratula() {
     cout << "====================================================\n";
     cout << "   Proyecto Final - Base de Datos en C++\n";
     cout << "   Integración con repositorio:\n";
-    cout << "   https://github.com/damiancabrera123/apoyo-proyecto-final-.git\n";
+    
     cout << "   Código base utilizado:\n";
-    cout << "   https://github.com/NicSam369/ProyectoFinal.git\n";
+    
     cout << "----------------------------------------------------\n";
-    cout << "   Autor: Damián Gonzalo Cabrera Asillo\n";
-    cout << "   Curso: [tu curso]\n";
-    cout << "   Docente: [tu profesor]\n";
+    cout << "   Autores: \n";
+    cout << "   Almary Sharelly Choque Barreda\n";
+    cout << "   Samantha Ncole Huayapa Ccapatinta\n";
+    cout << "   Fernanda Gabriela Zuñiga Camacho\n";
+    cout << "   Damián Gonzalo Cabrera Asillo\n";
+    cout << "   Curso: CCOMP2-2\n";
+    cout << "   Docente: Manuel Eduardo Loaiza Fernandez\n";
+    cout << "   Alvaro Henry Mamani Aliaga\n";
     cout << "   Fecha: [fecha actual]\n";
     cout << "====================================================\n\n";
-    cout<<"//*******************************************************************************************************//"<<endl;
-    cout<<"                        Bienvenido a CrediBanco "<<endl;
-    cout <<"Su banco de confianza "<<endl;
+
+    cout << "====================================================\n\n";
+    cout <<"    CREDIBANCO\n";
+    cout <<"Su banco de confianza \n";;
     cout<< "Aqui podra tener sus cuentas dependiendo de sus necesidades "<<endl;
     cout<< "En nuestro banco usted tendra seguridad y planes de interes a largo plazo"<<endl ;
     
@@ -41,19 +47,19 @@ void Credibanco:: caratula() {
     cout<< "           Debito "<<endl;
     cout<< "           Credito "<<endl;
     cout<< "           Cuenta de ahorro "<<endl;
-
+    cout << "----------------------------------------------------\n";
     cout <<"    Podra hacer transacciones de tres tipos "<< endl ;
     cout<< "           Deposito  "<<endl;
     cout<< "           Retiro "<<endl;
     cout<< "           Prestamo "<<endl;
-
+    cout << "----------------------------------------------------\n";
     cout<< "    Como siguiente paso vamos a pedirle sus datos para guardarlos en el sistema y asi asignarle su grupo de cuenta "<< endl ;
     cout<< "    Aparte se le dara ciertos beneficios en caso de ser parte de ser usuario  "<< endl ;
     cout<< "           Normal  "<<endl;
     cout<< "           Medio  "<<endl;
     cout<< "           Premiun  "<<endl;
     cout<< "           Preferencial  "<<endl;
-
+    cout << "====================================================\n\n";
 
 }
 
@@ -73,6 +79,7 @@ Credibanco::~Credibanco()
 {
     //dtor
 }
+
 
 
 
